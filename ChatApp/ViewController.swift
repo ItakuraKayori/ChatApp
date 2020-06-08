620c6af42742649aa4cb7acb26f86ec0cb1031d7
ItakuraKayori <mamedaifuku2525@gmail.com>
2020-06-08 23:47:38 +0900

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by 板倉果保 on 2020/06/08.
+//  Copyright © 2020 Itakura Kayori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
