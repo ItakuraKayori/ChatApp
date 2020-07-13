368cc9e8050668a2133efa965611f9da35920edd
ItakuraKayori <mamedaifuku2525@gmail.com>
2020-07-13 11:10:37 +0900

    first commit

---
+++ ChatApp/Model/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  ChatApp
+//
+//  Created by 板倉果保 on 2020/06/12.
+//  Copyright © 2020 Itakura Kayori. All rights reserved.
+//
+
+import Foundation
---
+++ ChatApp/View/CustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCell.swift
+//  ChatApp
+//
+//  Created by 板倉果保 on 2020/06/10.
+//  Copyright © 2020 Itakura Kayori. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
